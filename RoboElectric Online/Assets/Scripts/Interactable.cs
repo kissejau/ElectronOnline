cc1403cb1288105b67e0745942614bb6b7c6fea5
kissejau <40569704+kissejau@users.noreply.github.com>
2022-03-21 14:16:43 +0300

    Delete RoboElectric Online/Assets directory

--- RoboElectric Online/Assets/Scripts/Interactable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Mirror;
-
- public class Interactable : NetworkBehaviour
-{     
-     public virtual void Interaction(NetworkIdentity gameObject){}
-}
